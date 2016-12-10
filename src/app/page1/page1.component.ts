feb489b786fd4d93b77a7a0ff3e6532bdb137b22
Martin Casey <x00119025@gmail.com>
2016-12-10 12:15:41 +0000

    Finished anglur2Routing tutorial.

--- src/app/page1/page1.component.ts
+++ src/app/page1/page1.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { Router } from "@angular/router";
+ 
 @Component({
-  selector: 'app-page1',
-  templateUrl: './page1.component.html',
-  styleUrls: ['./page1.component.css']
+    selector: 'app-page1',
+    templateUrl: './page1.component.html',
+    styleUrls: ['./page1.component.css']
 })
-export class Page1Component implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class Page1Component {
+ 
+    constructor(private router: Router) { }
+ 
+    navigate() {
+        this.router.navigate(["page2"]);
+    }
+ 
 }
